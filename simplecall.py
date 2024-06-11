6cee3d3d73463ba221886a5107a2d692d74afaa3
Basil Gregory <bslgrgr@gmail.com>
2024-06-10 20:27:45 -0700

    specify that Discover Canada will be used; udpate script to use only part of request-doc

--- simplecall.py
+++ simplecall.py
@@ -3,12 +3,6 @@ import json
 from openai import OpenAI
 from dotenv import load_dotenv
 
-def split_by_first_occurrence(text, delimiter):
-    parts = text.split(delimiter, 1)
-    before_delimiter = parts[0]
-    after_delimiter = parts[1] if len(parts) > 1 else ''
-    return before_delimiter, after_delimiter
-
 # Load environment variables from .env file
 load_dotenv()
 
@@ -24,12 +18,7 @@ with open(current_dir + '/system-role.txt', 'r') as system_content_file:
 
 # Load the document content
 with open(current_dir + '/request-doc.txt', 'r') as request_file:
-    document_content = request_file.read()
-
-document_content_short, _ = split_by_first_occurrence(document_content, "Page: 9")
-document_content_1st_part, document_content_2nd_part = split_by_first_occurrence(document_content, "Page: 30")
-
-document_content = document_content_1st_part
+    document_lines = request_file.readlines()
 
 # Load previously generated questions
 questions_file_path = current_dir + '/questions.json'
@@ -39,15 +28,12 @@ if os.path.exists(questions_file_path):
 else:
     previous_questions = {"questions": []}
 
-def generate_new_question(system_content, document_content, previous_questions):
+def generate_new_questions(system_content, part_text):
     request = f"""
-    Based on the following document, generate as many new and unique questions as possible that have not been asked before:
+    Based on the following provided text, generate a few new and unique questions:
 
-    Document:
-    {document_content}
-
-    Previously Generated Questions:
-    {json.dumps(previous_questions, indent=2)}
+    Provided Text:
+    {part_text}
     """
 
     completion = client.chat.completions.create(
@@ -66,24 +52,51 @@ def save_questions(questions, file_path):
     with open(file_path, 'w') as questions_file:
         json.dump(questions, questions_file, indent=2)
 
+def process_part(start_line):
+    part_text = []
+    for i in range(start_line, len(document_lines)):
+        line = document_lines[i].strip()
+        if line.startswith("Page:"):
+            if part_text:
+                break
+            part_text.append(line)
+        elif line:
+            part_text.append(line)
+    return start_line, "\n".join(part_text)
+
 # Main loop to generate and confirm new questions
-while True:
-    new_questions = generate_new_question(system_content, document_content, previous_questions)
-
-    for question in new_questions['questions']:
-        print("\nNew Question Generated:")
-        print(json.dumps(question, indent=2))
-
-        user_input = input("Do you want to add this question? (Press Enter for yes, 'n' for no): ").strip().lower()
-        if user_input != 'n':
-            previous_questions['questions'].append(question)
-            save_questions(previous_questions, questions_file_path)
-            print("Question added.")
-        else:
-            print("Question skipped.")
-
-    user_input = input("Do you want to generate more questions? (Press Enter for yes, 'n' for no): ").strip().lower()
-    if user_input == 'n':
+current_line = 0
+while current_line < len(document_lines):
+    current_line, part_text = process_part(current_line)
+    if not part_text:
         break
+
+    print("\nCurrent Part of Text:")
+    print(part_text)
+
+    while True:
+        new_questions = generate_new_questions(system_content, part_text)
+
+        for question in new_questions['questions']:
+            print("\nNew Question Generated:")
+            print(json.dumps(question, indent=2))
+
+            user_input = input("Do you want to add this question? (Press Enter for yes, 'n' for no): ").strip().lower()
+            if user_input != 'n':
+                previous_questions['questions'].append(question)
+                save_questions(previous_questions, questions_file_path)
+                print("Question added.")
+            else:
+                print("Question skipped.")
+
+        user_input = input("Do you want to generate more questions on this part? (Press Enter for yes, 'n' for no): ").strip().lower()
+        if user_input == 'n':
+            break
+
+    user_input = input("Go to the next part (Press Enter), or finish (enter 'f'): ").strip().lower()
+    if user_input == 'f':
+        break
+
+    current_line += 1
 
 print("Finished generating questions.")
